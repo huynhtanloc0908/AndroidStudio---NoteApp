2627bdab0c2266965e77c16545b74d0e7b053cbf
huynhtanloc0908 <tanlochuynh112@gmail.com>
2025-10-04 12:57:52 +0700

    Update

---
+++ NoteApp/app/src/main/java/com/example/noteapp/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.noteapp;
+
+import android.os.Bundle;
+import android.content.Intent;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+public class MainActivity extends AppCompatActivity {
+    FloatingActionButton fabAddNote;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        // Ánh xạ nút FAB
+        fabAddNote = findViewById(R.id.fabAdd);
+        fabAddNote.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, AddNoteActivity.class);
+            startActivity(intent);
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
