ba643a494ad6050d2f843fc006c2c7c56ec19286
NghiaNT05 <nguyennghia11042005@gmail.com>
2025-10-06 12:45:49 +0700

    add back to  main buttton

--- NoteApp/app/src/main/java/com/example/noteapp/AddNoteActivity.java
+++ NoteApp/app/src/main/java/com/example/noteapp/AddNoteActivity.java
@@ -1,6 +1,7 @@
 package com.example.noteapp;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class AddNoteActivity extends AppCompatActivity {
 
@@ -8,6 +9,7 @@ public class AddNoteActivity extends AppCompatActivity {
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_add_note);
-
+            ImageButton btnBack = findViewById(R.id.btnBack);
+            btnBack.setOnClickListener(v -> finish());
         }
     }
